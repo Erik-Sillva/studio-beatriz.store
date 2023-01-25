b7fedc7786b9ac69b19f99f6c1b84bcab845e451
Erik Silva <104636199+Erik-Sillva@users.noreply.github.com>
2023-01-25 17:02:41 -0300

    sll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,57 @@
-// Toggle Menu
-let menutoggle = document.querySelector('.toggle');
-let containerNav = document.querySelector('.container-nav');
+// // Toggle Menu
+// let menutoggle = document.querySelector('.toggle');
+// let containerNav = document.querySelector('.container-nav');
 
-menutoggle.addEventListener("click", () => {
-    menutoggle.classList.toggle('active-menu');
+// menutoggle.addEventListener("click", () => {
+//     menutoggle.classList.toggle('active-menu');
 
-    if (window.screen.width <= 1000) {
-        if (menutoggle.classList.contains('active-menu')) {
-            containerNav.style.left = '0'
-        } else {
-            containerNav.style.left = '-12em'
-        }
-    }
-});
+//     if (window.screen.width <= 1000) {
+//         if (menutoggle.classList.contains('active-menu')) {
+//             containerNav.style.left = '0'
+//         } else {
+//             containerNav.style.left = '-12em'
+//         }
+//     }
+// });
 
-// Carrousel
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3.5,
-    spaceBetween: 19,
-    freeMode: true,
+// // Carrousel
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3.5,
+//     spaceBetween: 19,
+//     freeMode: true,
 
-    breakpoints: {
-        320: {
-            slidesPerView: 1.6,
-        },
+//     breakpoints: {
+//         320: {
+//             slidesPerView: 1.6,
+//         },
 
-        768: {
-            slidesPerView: 2.2,
-        },
+//         768: {
+//             slidesPerView: 2.2,
+//         },
 
-        900: {
-            slidesPerView: 2.8,
-        },
+//         900: {
+//             slidesPerView: 2.8,
+//         },
 
-        1200: {
-            slidesPerView: 3.5,
-        },
-    },
-});
+//         1200: {
+//             slidesPerView: 3.5,
+//         },
+//     },
+// });
 
 const openWindow = (penteado, valorAtual, valorNormal) => {
-    window.open('agendar-horario.html', 'Agendar Horário', 'width=800,height=900')
+    if (window.location.href === 'http://127.0.0.1:5500/index.html') {
+        window.location.assign('http://127.0.0.1:5500/agendar-horario.html')
+    }
     setTimeout(() => {
         info(penteado, valorAtual, valorNormal)
     }, 5000);
 }
 
-function info(penteado, valorAtual, valorNormal) {
-    if (window.location.pathname === '/agendar-horario.html') {
-        console.log(window.location.href)
-        document.querySelector('.penteado').innerHTML = modelo;
-        document.querySelector('.valor').innerHTML = valor;
+function info(a, b, c) {
+    if (window.location.href === 'http://127.0.0.1:5500/agendar-horario.html') {
+        console.log('Deu certo');
     } else {
-        console.log('Erro, URL encontrada: ', window.location.href)
+        console.log('Deu errado');
     }
 }
